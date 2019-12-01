0e90128b4d385d014a471ec74e2d4b986df9839e
Steve Fortier <mrstevefortier@gmail.com>
2019-11-30 19:31:34 -0500

    adding option to continue training

--- scripts/pose/train_g1_256p.sh
+++ scripts/pose/train_g1_256p.sh
@@ -3,4 +3,4 @@ python train.py --name pose2body_256p_g1 \
 --openpose_only --input_nc 3 --ngf 64 --num_D 2 \
 --resize_or_crop randomScaleHeight_and_scaledCrop --loadSize 384 --fineSize 256 --display_winsize 256 \
 --niter 5 --niter_decay 5 \
---no_first_img --n_frames_total 12 --max_frames_per_gpu 4 --max_t_step 4
+--no_first_img --n_frames_total 12 --max_frames_per_gpu 4 --max_t_step 4 $1

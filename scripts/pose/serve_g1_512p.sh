f98ee74b77d4b9c624bd7b4d59c8dda92efa0dd4
Steve Fortier <stevefortier@ntopology.com>
2019-12-04 13:45:39 -0500

    remove the server code from vid2vid

--- scripts/pose/serve_g1_512p.sh
+++
@@ -1,4 +0,0 @@
-python serve.py --name pose2body_512p_g1 \
---dataroot datasets/pose --dataset_mode pose \
---openpose_only --input_nc 3 --n_scales_spatial 2 --ngf 64 \
---loadSize 512 --no_first_img --port $1
